faf4e896055db499ade8976440697f0c88c33730
kshaikfaruk <39298818+kshaikfaruk@users.noreply.github.com>
2021-01-28 07:13:51 +0530

    first commit

---
+++ Project_demo/src/test/java/Testcase/SecondCalender.java
@@ -0,0 +1,41 @@
+package Testcase;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import pages.PageLocators;
+import pagesactions.PageActions;
+
+public class SecondCalender {
+	 PageActions page= new PageActions();
+	 @Test(priority=1)
+     public void click_on_Secondcalender(){
+   	  page.click_on_calender(PageLocators.second_date);
+     }
+    
+    @Test(priority=2)
+     public void select_year(){
+   page.clickonyear(PageLocators.secondyear);
+   	 page.clickonyear(PageLocators.second_year);
+    }
+    
+    @Test(priority=3)
+     public void select_month_secondcalender(){
+   	  page.select_month(PageLocators.month_next);
+     }
+    @Test(priority=4)
+	    public void click_ondate_secondcalender(){
+	    	page.click_ondate(PageLocators.second_date);
+	    }
+ 
+    @Test(priority=5)
+     public void click_calcutionduration(){
+   	 page.clickonresult(PageLocators.submit);
+    }
+    @Test(priority=6)
+    public void verify_result(){
+   	String current_result=page.verifyresult(PageLocators.verifydetails);
+   	 Assert.assertEquals(current_result, "Result: 1734 days");
+    }
+
+}
